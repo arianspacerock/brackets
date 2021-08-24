06486b20a7e47623aabbc8f0439bfe92bfe1045a
arianspacerock <siropemptyspace@gmail.com>
2021-08-24 14:38:23 +0300

    complete 100/100

--- src/index.js
+++ src/index.js
@@ -1,58 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-  let stack = [];
-
-    let lenstr = str.length;
-    if (lenstr % 2 != 0) return false;
-
-    let lenConfig = bracketsConfig.length;
-    let checkeven = 1;
-    let cheven;
-    let chClose;
-    for (let i = 0; i < lenstr; i++) {
-        let ch = str.charAt(i);
-        let close = false;
-        let open = false;
-        let even = false;
-        for (let j = 0; j < lenConfig; j++) {
-            if (bracketsConfig[j][0] == bracketsConfig[j][1] && ch == bracketsConfig[j][0]) {
-                even = true;
-                checkeven = (-1) * checkeven;
-                cheven = ch;
-                console.log('checkeven = ', checkeven)
-                break;
-            }
-            if (ch == bracketsConfig[j][0]) {
-                open = true;
-                break;
-            }
-            if (ch == bracketsConfig[j][1]) {
-                close = true;
-                chClose = bracketsConfig[j][0];
-                break;
-            }
-        }
-        if (even == true) {
-            if (checkeven == -1) {
-                stack.push(ch);
-                console.log('push=', ch);
-            }
-            if (checkeven == 1 && cheven == stack[stack.length - 1]) {
-                console.log(' pop stack[stack.length-1]=', stack[stack.length - 1], ', ch= ', ch)
-                stack.pop();
-            }
-        } else {
-
-            if (open === true) {
-                stack.push(ch);
-                console.log('push=', ch);
-            }
-            if (close === true && chClose == stack[stack.length - 1]) {
-                console.log(' pop stack[stack.length-1]=', stack[stack.length - 1], ', ch= ', ch)
-                stack.pop();
-            }
-        }
+    // your solution
+    let strTmp;
+    while (str) {
+      bracketsConfig.forEach(bracketsPair => str = str.replace(bracketsPair.join(''), ''))
+      if (str === strTmp) {
+         return false;
+      }
+      strTmp = str;
     }
-    return (stack.length === 0);
-};
-
+    return str.length === 0;
+  }
